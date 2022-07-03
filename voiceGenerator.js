257993566e49cdb634452fdfd993b752c0d72316
bin.malyi <greezyfire@gmail.com>
2022-07-03 22:06:31 +0300

    Join-to-Create discord generator

---
+++ voiceGenerator.js
@@ -0,0 +1,31 @@
+import { Collection } from 'discordjs';
+
+const voiceCollection = new Collection();
+
+// Pass the instance of Client as you defined it in your code
+client.on('voiceStateUpdate', async (oldState, newState) => {
+
+    if(!oldState.channel && newState.channel.id === `CHANNEL_ID`){
+        let user = await this.client.users.fetch(newState.id);
+        let option = {
+            type: 'GUILD_VOICE',
+            parent: newState.channel.parent,
+            permissionOverwrites: [
+                {
+                    id: newState.id,
+                    allow: ['MANAGE_CHANNELS','MANAGE_ROLES','MUTE_MEMBERS','DEAFEN_MEMBERS','MOVE_MEMBERS']
+                }
+            ]
+        }
+        let channel = await newState.guild.channels.create(user.tag,option).catch(error => console.error(error));
+
+        newState.member.voice.setChannel(channel);
+        voiceCollection.set(user.id,channel.id);
+
+    } else if(!newState.channel){
+        if(oldState.channelId === voiceCollection.get(newState.id)){
+            oldState.channel.delete();
+        }
+    }
+
+})
